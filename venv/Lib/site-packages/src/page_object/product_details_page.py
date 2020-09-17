8455ddffc0a8a64e9e7a10f88cce86db79fe22d6
KanikaGupta <pooja10404@gmail.com>
2020-09-17 18:26:40 +0530

    Delete all files

--- venv/Lib/site-packages/src/page_object/product_details_page.py
+++
@@ -1,9 +0,0 @@
-from src.common import webdriver_utility
-
-product_description = '//p[contains(text(),\'desc\')]'
-
-
-# Verification of the presence of item description.
-def verify_description_should_be_present(description):
-    text = webdriver_utility.get_text('XPATH', product_description.replace('desc', description))
-    assert text == description
