48d86f013476c9a45a5ee387dfe7f826171c4d35
KanikaGupta <pooja10404@gmail.com>
2020-09-17 18:29:46 +0530

    Added order process tests as given and please refer README.txt for same

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+# from selenium import webdriver
+#
+# def __init__(self):
+#     self.driver = webdriver.Chrome(
+#         executable_path="D:\\Huma_project/newhuma/HumaAndroid/tools/Windows/chromedriver.exe")
+#
+#
+# def setup_module(self):
+#     self.driver.maximize_window()
+#     self.driver.get("http://yahoo.com")
+#
+#
+# def setup_module(self):
+#     self.driver.close()
+#
+#
+# def get_driver(self):
+#     return self.driver
